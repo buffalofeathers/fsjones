c538201c94912bc0f63fbfc97bd63454b916214f
Jesse Payne <jessepayne@gmail.com>
2017-03-21 16:23:08 -0500

    latest update 3/21/17

---
+++ client/app.js
@@ -0,0 +1,47 @@
+angular.module("FSJones", ["ngRoute", "ngResource", "FSJones.controllers", "FSJones.services"])
+
+.config(["$routeProvider", "$locationProvider", function($routeProvider, $locationProvider) {
+    $locationProvider.html5Mode(true);
+    $routeProvider
+
+    .when("/", {
+        templateUrl: "views/welcome.html",
+        controller: "WecomeController"
+    })
+    .when("/team", {
+        templateUrl: "views/team.html"
+    })
+    .when("/teamone", {
+        templateUrl: "views/teamone.html"
+    })
+    .when("/teamtwo", {
+        templateUrl: "views/teamtwo.html"
+    })
+    .when("/services", {
+        templateUrl: "views/services.html"
+    })
+    .when("/forms", {
+        templateUrl: "views/forms.html",
+        controller: "FormsController"
+    })
+    .when("/refinanceform", {
+        templateUrl: "views/refinanceform.html",
+        controller: "FormsController"
+    })
+    .when("/purchaseform", {
+        templateUrl: "views/purchaseform.html",
+        controller: "FormsController"
+    })
+    .when("/contact", {
+        templateUrl: "views/contact.html",
+        controller: "ContactController"
+    })
+    .when("/glossary", {
+        templateUrl: "views/glossary.html"
+    })
+    .otherwise({
+        redirectTo: "/"
+    });
+}]);
+
+//seo services
